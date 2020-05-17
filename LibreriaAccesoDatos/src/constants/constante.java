9577af54ca81530a88c1413b33c5b32b91f9a0c2
Nelson Mosquera <nelson.a.mosquera@gmail.com>
2020-05-17 16:16:41 -0500

    Resolves drivers bugs

--- LibreriaAccesoDatos/src/constants/constante.java
+++ LibreriaAccesoDatos/src/constants/constante.java
@@ -12,5 +12,5 @@ package constants;
 public class constante {
     public static final String USERNAME = "is201702";
     public static final String PASSWORD = "tcK66x8wO0";
-    public static final String THINCONN = "jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDB [is201702 on IS201702]";
+    public static final String THINCONN = "jdbc:oracle:thin:@orion.javeriana.edu.co:1521:PUJDB";
 }
