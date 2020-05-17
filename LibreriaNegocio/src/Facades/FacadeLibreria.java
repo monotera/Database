aadbe7e919ea55863555376cb0a6ae4afab43200
Julian Camilo Builes Serrano <juliancho147@users.noreply.github.com>
2020-05-16 21:18:31 -0500

    Mejora a librerias

---
+++ LibreriaNegocio/src/Facades/FacadeLibreria.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Facades;
+
+import Interfaces.IFacadeLibreria;
+import entities.Libro;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author USER
+ */
+public class FacadeLibreria implements IFacadeLibreria{
+    private ArrayList<Libro> libros = new ArrayList<>();
+
+    public ArrayList<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(ArrayList<Libro> libros) {
+        this.libros = libros;
+    }
+    
+    @Override
+    public ArrayList<Libro> consultarLibros() {
+        return  this.libros;
+    }
+
+   
+
+    @Override
+    public void agregarLibro(Libro libro) {
+        this.libros.add(libro);
+    }
+    
+    
+}
